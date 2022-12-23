6310c470ab780381d2c581d80e0a41df2b6ad5d3
xyseer <xyseer2@gmail.com>
2022-12-23 13:17:01 +0800

    add parameters

---
+++ dao/IEItem.py
@@ -0,0 +1,8 @@
+class IEItem:
+    def __init__(self,id=-1,name=""):
+        self.id=id
+        self.name=name
+    def fetch(self):
+        pass
+    def push(self):
+        pass
---
+++ parameters.py
@@ -0,0 +1,72 @@
+import json
+from dao.IEItem import IEItem
+import os
+
+class Parameters(IEItem):
+    def __init__(self):
+        self.DB_PATH="/config/XNT.db"
+        self.ARIA2_RPC_SERVER="http://127.0.0.1:6800/jsonrpc"
+        self.ARIA2_JSONRPC_TOKEN=""
+        self.LOG_DIR="/config/logs/"
+        self.LOG_LEVEL="ERROR"
+        self.JACKETT_API_LINK_LIST=[]
+        self.ERROR_RETRY_SPAN=2
+        self.REGULAR_CHECK_SPAN=6
+        self.FILTER_DICT={"default":{"reject_rules":["720"],"apply_rules":["1080"]}}
+        self.other={}
+        super().__init__()
+
+    def fetch(self,file_path="/config/config.json"):
+        if not os.path.exists("/config"):
+            os.mkdir("/config")
+        if not os.path.exists(file_path):
+            open(file_path, "w").close()
+        with open(file_path,"r",encoding="utf8") as fp:
+            try:
+                paras_json = json.load(fp)
+            except Exception:
+                paras_json = ""
+        if not paras_json:
+            from dto.SqliteDB import DBManipulator
+            from logging_module import Logger
+            if not os.path.exists(self.LOG_DIR):
+                os.mkdir(self.LOG_DIR)
+                Logger(self.LOG_DIR,self.LOG_LEVEL).error("No Valid Config, using default.")
+            DBManipulator(self.DB_PATH)
+            self.push(file_path)
+        else:
+            self.DB_PATH = paras_json.get('DB_PATH', self.DB_PATH)
+            self.LOG_DIR = paras_json.get('LOG_DIR', self.LOG_DIR)
+            self.LOG_LEVEL = paras_json.get('LOG_DIR', self.LOG_LEVEL)
+            self.ERROR_RETRY_SPAN = tryParseInt(paras_json.get('ERROR_RETRY_SPAN', self.ERROR_RETRY_SPAN))
+            self.REGULAR_CHECK_SPAN = tryParseInt(paras_json.get('REGULAR_CHECK__SPAN', self.REGULAR_CHECK_SPAN))
+            self.ARIA2_RPC_SERVER = paras_json.get('ARIA2_RPC_SERVER', self.ARIA2_RPC_SERVER)
+            self.ARIA2_JSONRPC_TOKEN = paras_json.get('ARIA2_JSONRPC_TOKEN', self.ARIA2_JSONRPC_TOKEN)
+            self.JACKETT_API_LINK_LIST = paras_json.get('JACKETT_API_LINK_LIST', self.JACKETT_API_LINK_LIST)
+            self.FILTER_DICTS = paras_json.get('FILTER_DICTS', self.FILTER_DICTS)
+            self.other = paras_json.get('other',self.other)
+
+    def push(self,file_path="/config/config.json"):
+        with open(file_path, "w") as fp:
+            paras_json = {
+                'DB_PATH': self.DB_PATH,
+                'LOG_DIR': self.LOG_DIR,
+                'LOG_LEVEL': self.LOG_LEVEL,
+                'ERROR_RETRY_SPAN': self.ERROR_RETRY_SPAN,
+                'REGULAR_CHECK_SPAN': self.REGULAR_CHECK_SPAN,
+                'ARIA2_RPC_SERVER': self.ARIA2_RPC_SERVER,
+                'ARIA2_JSONRPC_TOKEN': self.ARIA2_JSONRPC_TOKEN,
+                'JACKETT_API_LINK_LIST': self.JACKETT_API_LINK_LIST,
+                'FILTER_DICTs': self.FILTER_DICTS,
+                'other': self.other
+            }
+            json.dump(paras_json, fp)
+            from logging_module import Logger
+            Logger().info("Parameters are Saved.")
+
+def tryParseInt(input:str)->int:
+    try:
+        return int(input)
+    except ValueError:
+        return 0
+
