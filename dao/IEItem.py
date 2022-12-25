87008cbabefcb4853ddae186f6b07b206a84a4c5
xyseer <xyseer2@gmail.com>
2022-12-25 22:27:36 +0800

    add return type in IEItem Interface

--- dao/IEItem.py
+++ dao/IEItem.py
@@ -1,8 +1,15 @@
+from typing import TypeVar
+
+_T = TypeVar('_T', bound="IEItem")
+
+
 class IEItem:
-    def __init__(self,id=-1,name=""):
-        self.id=id
-        self.name=name
-    def fetch(self):
-        pass
-    def push(self):
-        pass
+    def __init__(self, id=-1, name=""):
+        self.id = id
+        self.name = name
+
+    def fetch(self) -> _T:
+        return self
+
+    def push(self) -> _T:
+        return self
