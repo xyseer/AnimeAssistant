e40b5e6dd751fc41f56f513168a1312c0ae59c16
xyseer <xyseer2@gmail.com>
2022-12-26 17:06:44 +0800

    add list-str tools

--- dto/dbTools.py
+++ dto/dbTools.py
@@ -14,3 +14,14 @@ def convert_datetime(val: bytes):
 
 def convert_datetime(val: str):
     return datetime.strptime(val, UNIFIED_TIME_FORMAT)
+
+def listTostr(l:list,sep:str=";"):
+    result=""
+    for x in l[0:-1]:
+        result+=x+sep
+    result+=l[-1]
+    return result
+def strTolist(s:str,sep:str=";"):
+    result = s.split(";")
+    result.remove("") if "" in result else result
+    return result
