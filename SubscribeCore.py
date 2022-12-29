2e90351308fbb3ea91f5c7f4ef39af8af2f1e19c
xyseer <xyseer2@gmail.com>
2022-12-29 12:03:23 +0800

    pass the single-subscription test

--- SubscribeCore.py
+++ SubscribeCore.py
@@ -1,20 +1,18 @@
 from dto import dbTools
 from dao.dataItems import *
-from multiprocessing import process, pool
 from download_tools.IEDownloadMethod import IEDownloadMethod
 from datetime import datetime, timedelta
 from parameters import Parameters
-from GLOBAL_DEFINE import UNIFIED_TIME_FORMAT
 from logging_module import Logger
-
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.schedulers import SchedulerNotRunningError, SchedulerAlreadyRunningError
+from tzlocal import get_localzone
 
 
 class SubscribeCore:
     def __init__(self):
         self.download_items = dbTools.getDownloadItems()
-        self.scheduler = BackgroundScheduler()
+        self.scheduler = BackgroundScheduler(timezone=str(get_localzone()) if get_localzone()!=None else "UTC")
         self.remap_scheduler()
 
     def _fetch_items(self):
@@ -25,15 +23,15 @@ class SubscribeCore:
         self.shutdown_scheduler()
         self.scheduler.remove_all_jobs()
         p = Parameters()
-        self.scheduler.add_job(self.remap_scheduler, "interval", hours=p.REGULAR_CHECK_SPAN)
+        self.scheduler.add_job(self.remap_scheduler, "interval", hours=p.REGULAR_CHECK_SPAN,id="remap_scheduler")
         for i in self.download_items:
             if i.nextUpdateTime < datetime.now():
-                self.scheduler.add_job(self.check_update_done, args=[i])
+                self.scheduler.add_job(self._check_update_done, args=[i],id=str(i.id))
             else:
-                self.scheduler.add_job(self.check_update_done, "date", run_date=i.nextUpdateTime, args=[i])
+                self.scheduler.add_job(self._check_update_done, "date", run_date=i.nextUpdateTime, args=[i],id=str(i.id))
         self.start_scheduler()
 
-    def check_update_done(self, download_item: DownloadItem, times: int = 1):
+    def _check_update_done(self, download_item: DownloadItem, times: int = 1):
         if times <= 0:
             return
         p = Parameters()
@@ -44,17 +42,18 @@ class SubscribeCore:
             else:
                 return
         else:
-            if times < 10:
-                span = 1 if int(p.ERROR_RETRY_SPAN / times) <= 0 else int(p.ERROR_RETRY_SPAN / times)
-                self.scheduler.add_job(self.check_update_done, "interval", hours=span,
-                                       args=[download_item, times + 1])
+            if times <= 5:
+                span_minutes = 1 if int(p.ERROR_RETRY_SPAN * 60 / times) <= 0 else int(p.ERROR_RETRY_SPAN * 60 / times)
+                self.scheduler.add_job(self._check_update_done, "date",
+                                       run_date=datetime.now() + timedelta(minutes=span_minutes),
+                                       args=[download_item, times + 1],id=f"{download_item.id}_retry_{times+1}")
                 Logger().warning(
                     f"Subscription {download_item.name} has failed for {times} times. Please check the infos are all correct.")
                 return
             else:
                 span = int(p.ERROR_RETRY_SPAN * 20)
                 Logger().error(
-                    f"Subscription {download_item.name} has error fetched for 10 times. This Subscription forced to delay for {span} hours.")
+                    f"Subscription {download_item.name} has error fetched for 5 times. This Subscription forced to delay for {span} hours.")
                 download_item.nextUpdateTime = download_item.nextUpdateTime + timedelta(hours=span)
                 download_item.push()
                 return
@@ -81,8 +80,12 @@ class SubscribeCore:
         if len(type_name.split(".")) < 2:
             type_name = "download_tools." + type_name
         class_name = type_name.split(".")[-1]
-        return getattr(getattr(__import__(type_name), class_name, IEDownloadMethod), class_name, IEDownloadMethod)(
+        try:
+            return getattr(getattr(__import__(type_name), class_name, IEDownloadMethod), class_name, IEDownloadMethod)(
             download_item)
+        except ModuleNotFoundError or AttributeError:
+            Logger().error(f"{download_item.type} Not Found. Please check the extensions.")
+            return IEDownloadMethod(download_item)
 
     @staticmethod
     def single_item_subscribe(download_item: DownloadItem) -> bool:
