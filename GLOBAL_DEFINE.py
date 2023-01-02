757cf9b5cd065b7df18828a6c5fd13f7707ff59e
xyseer <xyseer2@gmail.com>
2023-01-02 21:40:13 +0800

    final commit for 0.99_b4

--- GLOBAL_DEFINE.py
+++ GLOBAL_DEFINE.py
@@ -1,6 +1,6 @@
-CONFIG_DIR="./config"
+CONFIG_DIR="/config"
 CONFIG_FILE=CONFIG_DIR+"/config.json"
 UNIFIED_TIME_FORMAT="%Y-%m-%d %H:%M"
-VERSION_INFO="0.99_beta_01"
+VERSION_INFO="0.99_beta_04"
 HTML_TIME_FORMAT = "%Y-%m-%d\n%H:%M"
 HTML_INPUT_TIME_FORMAT = "%Y-%m-%dT%H:%M"
