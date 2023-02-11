ed43b2292ee965d72c7688b293eeeefc0f083430
xyseer <xyseer2@gmail.com>
2023-02-11 10:27:11 +0800

    update version_info to 0.99_beta_09

--- GLOBAL_DEFINE.py
+++ GLOBAL_DEFINE.py
@@ -1,7 +1,7 @@
 CONFIG_DIR = "/config"
 CONFIG_FILE = CONFIG_DIR + "/config.json"
 UNIFIED_TIME_FORMAT = "%Y-%m-%d %H:%M"
-VERSION_INFO = "0.99_beta_04"
+VERSION_INFO = "0.99_beta_09"
 HTML_TIME_FORMAT = "%Y-%m-%d\n%H:%M"
 HTML_INPUT_TIME_FORMAT = "%Y-%m-%dT%H:%M"
 DEBUG_MODE = True
